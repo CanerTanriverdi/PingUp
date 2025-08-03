7e5af20505c988576b5855cfb9f8100d9adbe0df
CanerTanriverdi <canertanriverdiis@gmail.com>
2025-08-03 19:55:13 +0300

    login page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Feed from "./pages/Feed";
@@ -8,50 +7,54 @@ import Connections from "./pages/Connections";
 import Discover from "./pages/Discover";
 import Profile from "./pages/Profile";
 import CreatePost from "./pages/CreatePost";
+import { useUser } from "@clerk/clerk-react";
+import Layout from "./pages/Layout";
 
 const App = () => {
-  return;
-  <>
-    <Routes>
-      <Route
-        path="/"
-        element={<Login />}
-      >
+  const { user } = useUser();
+  return (
+    <>
+      <Routes>
         <Route
-          index
-          element={<Feed />}
-        />
-        <Route
-          path="messages"
-          element={<Messages />}
-        />
-        <Route
-          path="messages/:userId"
-          element={<ChatBox />}
-        />
-        <Route
-          path="connections"
-          element={<Connections />}
-        />
-        <Route
-          path="discover"
-          element={<Discover />}
-        />
-        <Route
-          path="profile"
-          element={<Profile />}
-        />
-        <Route
-          path="profile/:profileId"
-          element={<Profile />}
-        />
-        <Route
-          path="create-post"
-          element={<CreatePost />}
-        />
-      </Route>
-    </Routes>
-  </>;
+          path="/"
+          element={!user ? <Login /> : <Layout />}
+        >
+          <Route
+            index
+            element={<Feed />}
+          />
+          <Route
+            path="messages"
+            element={<Messages />}
+          />
+          <Route
+            path="messages/:userId"
+            element={<ChatBox />}
+          />
+          <Route
+            path="connections"
+            element={<Connections />}
+          />
+          <Route
+            path="discover"
+            element={<Discover />}
+          />
+          <Route
+            path="profile"
+            element={<Profile />}
+          />
+          <Route
+            path="profile/:profileId"
+            element={<Profile />}
+          />
+          <Route
+            path="create-post"
+            element={<CreatePost />}
+          />
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
